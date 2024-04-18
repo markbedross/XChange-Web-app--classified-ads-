406daef66aca68b3c2170724c87b09a9c1ee3648
Mark <n01586869@humber.ca>
2024-04-18 13:16:45 -0400

    changed port to process env

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const adRoutes = require('./routes/adRoutes.js')
 const userRoutes = require('./routes/userRoutes.js')
 
-const PORT = 8000
+// const PORT = 8000
 
 // setting transfer limit to 50mb
 app.use(bodyParser.json({limit: '50mb'}));
@@ -20,6 +20,6 @@ app.use('/user', userRoutes)
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(()=>{
-    app.listen(PORT, ()=>console.log(`Listening on ${PORT}`))
+    app.listen(process.env.PORT, ()=>console.log(`Listening on ${PORT}`))
 })
 .catch (err => console.log(err))
